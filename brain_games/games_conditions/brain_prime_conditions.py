408b09df2ee59dc55fedf81a8a8bcdf95f95044f
Lyubov Rodina <kreizsasha@gmail.com>
2023-06-02 19:35:59 +0300

    update brain_prime_conditions.py

--- brain_games/games_conditions/brain_prime_conditions.py
+++ brain_games/games_conditions/brain_prime_conditions.py
@@ -4,17 +4,18 @@ from random import randint
 task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_simple(num):
+    if num < 2:
+        return False
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
 def question_answer():
     random_number = randint(0, 100)
     answer = None
-
-    def is_simple(num):
-        if num < 2:
-            return False
-        for i in range(2, (num//2) + 1):
-            if num % i == 0:
-                return False
-        return True
 
     if is_simple(random_number):
         answer = 'yes'
