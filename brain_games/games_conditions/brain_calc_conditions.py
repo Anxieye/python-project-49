b03d0a7030e476eaf76309a8cca79283fd78255b
Lyubov Rodina <kreizsasha@gmail.com>
2023-06-03 13:28:31 +0300

    change brain_calc_conditions.py

--- brain_games/games_conditions/brain_calc_conditions.py
+++ brain_games/games_conditions/brain_calc_conditions.py
@@ -4,10 +4,21 @@ from random import randint, choice
 TASK = 'What is the result of the expression?'
 
 
+def find_operator(a, b, operator):
+    value = None
+    if operator == '+':
+        value = a + b
+    elif operator == '-':
+        value = a - b
+    else:
+        value = a * b
+    return value
+
+
 def question_answer():
     random_operator = choice(('+', '-', '*'))
     random_number = randint(0, 100)
     random_number_2 = randint(0, 100)
     question = f'{random_number} {random_operator} {random_number_2}'
-    answer = str(eval(question))
+    answer = find_operator(random_number, random_number_2, random_operator)
     return [question, answer]
